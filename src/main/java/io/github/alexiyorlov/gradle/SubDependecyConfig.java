58e0a79257ea51524c84f6a355e6c78a069fab92
Alexiy <alexiy.ov@gmail.com>
2019-12-07 17:27:43 +0200

    First commit

---
+++ src/main/java/io/github/alexiyorlov/gradle/SubDependecyConfig.java
@@ -0,0 +1,172 @@
+package io.github.alexiyorlov.gradle;
+
+import org.gradle.api.Project;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+
+@SuppressWarnings("unused")
+public class SubDependecyConfig
+{
+    private Project parentProject;
+    private boolean doLogging = true;
+    private HashSet<String> excludedProjects;
+
+    private boolean simulate = true;
+    private boolean dependOnParent;
+    private HashSet<Project> subprojects;
+    private HashSet<String> passedConfigurations;
+
+    public SubDependecyConfig()
+    {
+        passedConfigurations = new HashSet<>(4);
+        excludedProjects = new HashSet<>(3);
+        subprojects = new HashSet<>(3);
+    }
+
+    /**
+     * Gradle build can have only one 'settings.gradle' file (in root project), so
+     * child projects of the subprojects must be included there to be affected
+     * by this plugin
+     */
+    public void addSubprojects(Collection<Project> subprojects)
+    {
+        this.subprojects.addAll(subprojects);
+    }
+
+    public void addSubprojects(Project... subprojects)
+    {
+        this.subprojects.addAll(Arrays.asList(subprojects));
+    }
+
+    /**
+     * Adds subprojects by name
+     */
+    public void addSubprojects(String... subprojects)
+    {
+        for (String subproject : subprojects)
+        {
+            for (Project subprjct : parentProject.getSubprojects())
+            {
+                if (subprjct.getName().equals(subproject))
+                {
+                    this.subprojects.add(subprjct);
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Adds subprojects by name
+     */
+    public void addSubProjects(Collection<String> names)
+    {
+        for (String name : names)
+        {
+            for (Project subproject : parentProject.getSubprojects())
+            {
+                if (subproject.getName().equals(name))
+                {
+                    this.subprojects.add(subproject);
+                    break;
+                }
+            }
+        }
+    }
+
+    public HashSet<Project> getSubprojects()
+    {
+        return subprojects;
+    }
+
+    /**
+     * Excludes projects by name
+     */
+    public void excludeProjects(String... excludedProjects)
+    {
+        this.excludedProjects.addAll(Arrays.asList(excludedProjects));
+    }
+
+    /**
+     * Excludes projects by name
+     */
+    public void excludeProjects(Collection<String> excludedProjects)
+    {
+        this.excludedProjects.addAll(excludedProjects);
+    }
+
+    public HashSet<String> getExcludedProjects()
+    {
+        return excludedProjects;
+    }
+
+    public HashSet<String> getPassedConfigurations()
+    {
+        return passedConfigurations;
+    }
+
+    /**
+     * Control what configurations will be passed to the subprojects (default is all)
+     *
+     * @param usedConfigurations names of dependency configurations
+     */
+    public void setPassedConfigurations(String... usedConfigurations)
+    {
+        this.passedConfigurations = new HashSet<>(Arrays.asList(usedConfigurations));
+    }
+
+    /**
+     * Set logging to console on or off
+     */
+    public void setDoLogging(boolean doLogging)
+    {
+        this.doLogging = doLogging;
+    }
+
+    public boolean doLogging()
+    {
+        return doLogging;
+    }
+
+    /**
+     * If true, then all actions are simulated (do not affect the project).
+     * Initially set to 'true'
+     */
+    public boolean isSimulated()
+    {
+        return simulate;
+    }
+
+    public void simulate(boolean simulate)
+    {
+        this.simulate = simulate;
+    }
+
+    /**
+     * Whether to add a dependency on the parent project to the subprojects
+     */
+    public void dependOnParent(boolean dependOnParent)
+    {
+        this.dependOnParent = dependOnParent;
+    }
+
+    public boolean isDependentOnParent()
+    {
+        return dependOnParent;
+    }
+
+    public Project getParentProject()
+    {
+        return parentProject;
+    }
+
+    /**
+     * Sets the parent project.
+     */
+    void setParentProject(Project project)
+    {
+        parentProject = project;
+    }
+}
---
+++ src/main/java/io/github/alexiyorlov/gradle/SubprojectDependencies.java
@@ -0,0 +1,89 @@
+package io.github.alexiyorlov.gradle;
+
+import com.google.common.collect.HashMultimap;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Dependency;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class SubprojectDependencies implements Plugin<Project>
+{
+    @Override
+    public void apply(Project project)
+    {
+        SubDependecyConfig subDependecyExtension = project.getExtensions().create("subprojectDeps", SubDependecyConfig.class);
+        subDependecyExtension.setParentProject(project);
+        if (subDependecyExtension.doLogging())
+            System.out.println("Applying " + SubprojectDependencies.class.getSimpleName());
+
+        subDependecyExtension.addSubprojects(project.getSubprojects());
+
+        HashSet<Project> subprojects = subDependecyExtension.getSubprojects();
+
+        if (subprojects.size() > 0)
+        {
+            Set<String> set = subprojects.stream().map(Project::getName).collect(Collectors.toSet());
+            if (subDependecyExtension.doLogging())
+                System.out.println("Detected sub-projects: " + set);
+
+            HashMultimap<String, Dependency> dependencyHashMultimap = HashMultimap.create();
+
+
+            project.afterEvaluate(project1 -> {
+
+                if (subDependecyExtension.doLogging())
+                {
+                    if (subDependecyExtension.isSimulated())
+                        System.out.println("(Simulation)");
+                    System.out.println("Gathering specified dependencies: ");
+                }
+                if (!subDependecyExtension.isSimulated())
+                {
+                    project1.getConfigurations().forEach(configuration -> {
+                        Set<String> usedConfigs = subDependecyExtension.getPassedConfigurations();
+                        if (usedConfigs.isEmpty() || usedConfigs.contains(configuration.getName()))
+                        {
+                            if (subDependecyExtension.doLogging())
+                                System.out.println("-> " + configuration.getName());
+                            configuration.getDependencies().forEach(dependency -> {
+                                dependencyHashMultimap.put(configuration.getName(), dependency);
+                            });
+                        }
+                    });
+                }
+            });
+
+            subprojects.forEach(subproject -> {
+                subproject.afterEvaluate(project1 -> {
+                    if (subDependecyExtension.getExcludedProjects().isEmpty() || !subDependecyExtension.getExcludedProjects().contains(project1.getName()))
+                    {
+                        if (subDependecyExtension.doLogging())
+                            System.out.println("Adding parent project dependencies to " + project1.getName());
+
+                        if (!subDependecyExtension.isSimulated())
+                        {
+                            if (subDependecyExtension.isDependentOnParent())
+                                project1.getDependencies().add("implementation", subDependecyExtension.getParentProject());
+                            dependencyHashMultimap.entries().forEach(stringDependencyEntry -> {
+                                project1.getDependencies().add(stringDependencyEntry.getKey(), stringDependencyEntry.getValue());
+                            });
+                        }
+                        if (subDependecyExtension.doLogging())
+                            System.out.println("Success");
+                    }
+                });
+            });
+
+        }
+        else
+        {
+            if (subDependecyExtension.doLogging())
+                System.out.println("No subprojects for project '" + project.getName() + "'");
+        }
+    }
+
+
+}
